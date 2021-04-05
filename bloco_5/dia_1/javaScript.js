a8d4d69d09f7dd2bcc5126326160352194d04c1c
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-05 20:13:53 -0300

    Fnalza exercicios 4 e 5

--- bloco_5/dia_1/javaScript.js
+++ bloco_5/dia_1/javaScript.js
@@ -1,11 +1,17 @@
-//E01
+//E01, E04
 function changeTagText(tag, position, text) {
   document.getElementsByTagName(tag)[position].textContent = text;
 }
 
+//E02, E03
 function changeBackgroundColor(specificClass, color) {
   let array = document.getElementsByClassName(specificClass);
   for (let index = 0; index < array.length; index += 1) {
     array[index].style.backgroundColor = color;
   }
+}
+
+//E05 
+function tagToUppercase(tag, position) {
+  document.getElementsByTagName(tag)[position].style.textTransform = 'uppercase';
 }
