546cf26ff1cc060a35c3e7c3179b909207948655
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-06 16:18:43 -0300

    Fecha exercicios do topico 2

--- bloco_5/dia_2/script.js
+++ bloco_5/dia_2/script.js
@@ -1,3 +1,4 @@
+// Tópico 1
 let e01 = document.getElementById('elementoOndeVoceEsta');
 console.log(e01);
 
@@ -21,3 +22,21 @@ console.log (e07);
 
 let e08 = e02.lastElementChild.previousElementSibling;
 console.log (e08);
+
+//Tópico 2
+let e09 = document.getElementById('pai');
+let tag = document.createElement('div');
+e09.appendChild(tag);
+
+let e10 = document.getElementById('elementoOndeVoceEsta');
+let tag2 = document.createElement('div');
+e10.appendChild(tag2);
+
+let e11 = document.getElementById('primeiroFilhoDoFilho')
+let tag3 = document.createElement('div');
+e11.appendChild(tag3);
+
+let e12 = document.getElementById('primeiroFilhoDoFilho').firstElementChild;
+console.log(e12.parentElement.parentElement.nextElementSibling);
+
+
