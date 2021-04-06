0993bc790284c37d99040fb73f0bcbdc40d0bd7a
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-06 19:00:59 -0300

    Resolve os desafios 1 e 2

---
+++ bloco_5/dia_2/exercises5_2.js
@@ -0,0 +1,10 @@
+//E01
+document.body.appendChild(document.createElement('h1')).id = 'title';
+
+document.getElementById('title').innerText = 'Exercício 5.2 - JavaScript DOM';
+
+//E02
+document.body.appendChild(document.createElement('div')).className = 'main-content';
+
+//E03
+
